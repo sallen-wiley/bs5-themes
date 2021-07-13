d2ad8ca675c7f3b6433c63136ea647cc8e3279fb
sallen-wiley <sallen@wiley.com>
2021-07-13 09:50:13 +0100

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var theme = "wji";
+
+const gulp = require("gulp");
+const cleanCSS = require('clean-css');
+const sass = require("gulp-sass");
+const browserSync = require("browser-sync").create();
+
+// gulp.task('sass', ()  => {
+//     return gulp.src("app/scss/*.scss")
+//         .pipe(sass())
+//         .pipe(gulp.dest("app/css"))
+//         .pipe(browserSync.stream());
+// });
+
+gulp.task('sass' + theme, ()  => {
+    return gulp.src("app/scss/" + theme + "/*.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("app/css"))
+        .pipe(gulp.dest("app/scss/" + theme + "/"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('start', gulp.series(['sass' + theme], function() {
+
+    browserSync.init({
+      server: "./app/",
+      index: "./index.html",
+    });
+
+    gulp.watch("app/scss/**/*.scss", gulp.series(['sass' + theme]));
+    gulp.watch("app/*.html").on('change', browserSync.reload);
+}));
+
+gulp.task('default', gulp.series('start'));
